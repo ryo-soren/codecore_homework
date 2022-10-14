b54b72dea3eb5169792fb906f9b17d12b20355a6
ryoS <ryosorensen0606@gmail.com>
2022-10-13 22:21:26 -0700

    adding view files

--- super_team_picker/routes/super_team_picker.js
+++ super_team_picker/routes/super_team_picker.js
@@ -3,7 +3,15 @@ const knex = require("../db/client")
 const router = express.Router()
 
 
-
+router.get("/", (req,res) => {
+    res.render("super_team/home")
+})
+router.get("/new", (req,res) => {
+    res.render("super_team/new")
+})
+router.get("/cohorts", (req,res) => {
+    res.render("super_team/cohorts")
+})
 
 
 module.exports = router
