ef48ac6a751b8d15038a340b7ddf6ec2e51a83d5
ryoS <ryosorensen0606@gmail.com>
2022-12-01 22:58:49 -0800

    Added a list with hardcoded words to choose from at random on each reload

--- hangman/js/index.js
+++ hangman/js/index.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
                 <button 
                     type="button" 
                     class="letter btn btn-outline-dark m-1"
-                    id = "${letter}"
-                    value = ${letter}
+                    id = "${letter.toLowerCase()}"
+                    value = "${letter.toLowerCase()}"
                     >${letter}</
                 button>
             `)
@@ -16,12 +16,22 @@ $(document).ready(function(){
     }
 
     function wordCheck() {
-        const word = ['B','L','O','B']
+
+        
+        const words = [
+            'chain', 'dog', 'belt', 
+            'crystal', 'protein', 'airplane', 
+            'soup', 'cathedral', 'mother', 
+            'father', 'cat', 'space', 'sky'
+        ]
+
+        const word = words[Math.floor(Math.random()*words.length)+1].split("")
+
         for (let index = 0; index < word.length; index++) {
             $(".guesses").append(`
                 <div 
-                class="my-4 guess ${word[index]}"
-                id="${index}">
+                class="my-4 guess"
+                id="${word[index]}">
                     ${" "}
                 </div>
             `);
@@ -32,12 +42,11 @@ $(document).ready(function(){
         let imgIndex = 1
 
         $(".letter.btn-outline-dark").click((event) => {
-            $(event.target).toggleClass("btn-dark text-white").css("pointer-events", "none")
             guesses++
-            const target = event.target.value
+            const target = event.target.id
             if (word.includes(target)) {
 
-                $(`.guess.${target}`).text(`${target}`) 
+                $(`.guess#${target}`).text(`${target}`) 
 
                 word.forEach(letter => {
                     if (letter === target) {
@@ -59,11 +68,12 @@ $(document).ready(function(){
                 }, 100)
             }else if (wrongGuesses === 6) {
                 setTimeout(()=>{
-                    if(confirm(("No more guesses! Press ok to play again"))){
+                    if(confirm((`No more guesses, the word was "${word.join("")}"! Press ok to play again`))){
                         location.reload()
                     }            
                 }, 100)
             }
+            $(event.target).toggleClass("btn-dark text-white").css("pointer-events", "none")
         })        
     }
 
